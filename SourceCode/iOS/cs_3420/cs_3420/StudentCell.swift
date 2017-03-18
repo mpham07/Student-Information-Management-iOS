049490757ebb822e2a66ecc619de4269e37aa54e
Minh Pham <ducminh.it48@gmail.com>
2017-03-18 12:49:06 -0500

    designed StudentVC

---
+++ SourceCode/iOS/cs_3420/cs_3420/StudentCell.swift
@@ -0,0 +1,24 @@
+//
+//  StudentCell.swift
+//  cs_3420
+//
+//  Created by Minh Pham on 3/18/17.
+//  Copyright © 2017 devmen. All rights reserved.
+//
+
+import UIKit
+
+class StudentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
