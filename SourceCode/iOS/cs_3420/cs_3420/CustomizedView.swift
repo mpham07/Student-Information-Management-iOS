b32c04584e98bcc7a7d3bc8b6bd4850c05e4d688
Minh Pham <ducminh.it48@gmail.com>
2017-04-02 22:04:50 -0500

    fixed bugs

---
+++ SourceCode/iOS/cs_3420/cs_3420/CustomizedView.swift
@@ -0,0 +1,19 @@
+//
+//  CustomizedView.swift
+//  cs_3420
+//
+//  Created by Minh Pham on 4/2/17.
+//  Copyright © 2017 devmen. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomizedView: UIView {
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet{
+            
+        }
+    }
+}
